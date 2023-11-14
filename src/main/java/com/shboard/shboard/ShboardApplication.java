808bb8a70d2c4aebb46474f580acb1983507c58a
sh111-coder <ohk9134@naver.com>
2023-11-14 10:03:24 +0900

    init project

---
+++ src/main/java/com/shboard/shboard/ShboardApplication.java
@@ -0,0 +1,13 @@
+package com.shboard.shboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShboardApplication.class, args);
+	}
+
+}
