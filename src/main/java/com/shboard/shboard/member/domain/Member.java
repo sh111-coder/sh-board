3515d5aaf57a97afcd5a8caed82ae40141621580
sh111-coder <ohk9134@naver.com>
2023-11-14 10:37:33 +0900

    feat: Member Entity 구현

---
+++ src/main/java/com/shboard/shboard/member/domain/Member.java
@@ -0,0 +1,29 @@
+package com.shboard.shboard.member.domain;
+
+import com.shboard.shboard.global.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+}
