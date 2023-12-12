5177dc1f2ce062960378ca491dfe9f4d6904124a
sh111-coder <ohk9134@naver.com>
2023-12-12 15:05:20 +0900

    feat: 페이지 검색 기능 구현

--- src/main/java/com/shboard/shboard/board/application/BoardService.java
+++ src/main/java/com/shboard/shboard/board/application/BoardService.java
@@ -1,8 +1,10 @@
 package com.shboard.shboard.board.application;
 
 import com.shboard.shboard.board.application.dto.BoardsResponse;
+import com.shboard.shboard.board.application.dto.ClientBoardSearchCondition;
 import com.shboard.shboard.board.domain.Board;
 import com.shboard.shboard.board.domain.BoardRepository;
+import com.shboard.shboard.board.domain.dto.BoardSearchCondition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,5 +23,13 @@ public class BoardService {
         final Page<Board> boardPage = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
 
         return BoardsResponse.of(boardPage, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public BoardsResponse searchByCondition(final ClientBoardSearchCondition condition, final Pageable pageable) {
+        final BoardSearchCondition conditionForRepo = new BoardSearchCondition(condition.title(), condition.writer());
+        final Page<Board> searchBoards = boardRepository.searchByCondition(conditionForRepo, pageable);
+
+        return BoardsResponse.of(searchBoards, pageable);
     }
 }
